370c19bfc5229617fd26c66af4e9ca7712f79c60
Dusky <7453269+Aliman999@users.noreply.github.com>
2023-08-29 12:19:54 -0400

    moved axios method

--- api/util/axios.js
+++ api/util/axios.js
@@ -14,30 +14,30 @@ const api = {
   run: async (url, payload = null) => {
     return new Promise(callback => {
       operation.attempt(() => {
-        axios((url), {
-          method: 'post',
-          headers: { 
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US,en;q=0.5', 
-            'User-Agent': "Mozilla/5.0",
-            'Cache-Control': 'no-cache',
-            'Cookie': "Rsi-Token="
-          },
-          data: payload ? payload : "",
-        }).then((result) => {
-          if(result.status != 200){
-            console.log(result);
-            process.exit(0);
-          }
-          try{
-            const $ = cheerio.load(payload ? result.data.data.html : result.data);
-            callback($);
-          }catch(e){
-            console.log("Fetch Error");
-            console.log(e);
-            operation.retry(e);
-          }
-        })
+        try{
+          axios((url), {
+            method: 'post',
+            headers: { 
+              'Accept': 'application/json',
+              'Accept-Language': 'en-US,en;q=0.5', 
+              'User-Agent': "Mozilla/5.0",
+              'Cache-Control': 'no-cache',
+              'Cookie': "Rsi-Token="
+            },
+            data: payload ? payload : "",
+          }).then((result) => {
+              const $ = cheerio.load(payload ? result.data.data.html : result.data);
+              callback($);
+            if(result.status != 200){
+              console.log(result);
+              process.exit(0);
+            }
+          })
+        }catch(e){
+          console.log("Fetch Error");
+          console.log(e);
+          operation.retry(e);
+        }
       });
       
     })
