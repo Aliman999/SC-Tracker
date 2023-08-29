31c262c01ca1733c47f4d87dc748258fac903860
Dusky <7453269+Aliman999@users.noreply.github.com>
2023-08-29 12:39:39 -0400

    changed try catch block

--- api/util/axios.js
+++ api/util/axios.js
@@ -14,30 +14,28 @@ const api = {
   run: async (url, payload = null) => {
     return new Promise(callback => {
       operation.attempt(() => {
-        try{
-          axios((url), {
-            method: 'post',
-            headers: { 
-              'Accept': 'application/json',
-              'Accept-Language': 'en-US,en;q=0.5', 
-              'User-Agent': "Mozilla/5.0",
-              'Cache-Control': 'no-cache',
-              'Cookie': "Rsi-Token="
-            },
-            data: payload ? payload : "",
-          }).then((result) => {
-              const $ = cheerio.load(payload ? result.data.data.html : result.data);
-              callback($);
-            if(result.status != 200){
-              console.log(result);
-              process.exit(0);
-            }
-          })
-        }catch(e){
+        axios((url), {
+          method: 'post',
+          headers: { 
+            'Accept': 'application/json',
+            'Accept-Language': 'en-US,en;q=0.5', 
+            'User-Agent': "Mozilla/5.0",
+            'Cache-Control': 'no-cache',
+            'Cookie': "Rsi-Token="
+          },
+          data: payload ? payload : "",
+        }).then((result) => {
+            const $ = cheerio.load(payload ? result.data.data.html : result.data);
+            callback($);
+          if(result.status != 200){
+            console.log(result);
+            process.exit(0);
+          }
+        }).catch((e) => {
           console.log("Fetch Error");
           //console.log(e);
           operation.retry(e);
-        }
+        })
       });
       
     })
