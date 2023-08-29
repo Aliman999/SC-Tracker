38cc3423847cfe4920c49432a52122a0e4ab675c
Dusky <7453269+Aliman999@users.noreply.github.com>
2023-08-29 14:07:40 -0400

    test

--- api/util/axios.js
+++ api/util/axios.js
@@ -23,6 +23,15 @@ const api = {
         const $ = cheerio.load(payload ? result.data.data.html : result.data);
         callback($);
       }).catch((e) => {
+        fs.writeFile("books.txt", data, (err) => {
+          if (err)
+            console.log(err);
+          else {
+            console.log("File written successfully\n");
+            console.log("The written has the following contents:");
+            console.log(fs.readFileSync("books.txt", "utf8"));
+          }
+        });
         console.log(e);
         callback(null);
       })
